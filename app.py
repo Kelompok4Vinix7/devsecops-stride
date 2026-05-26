4fbf37d9934838b1f4f0f70d1fde4d2b97c4e4cc
axel <axel@gmail.com>
2026-05-26 17:25:56 +0000

    Simulate malicious commit

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 @app.route('/')
 def home():
+    os.system("echo MALICIOUS COMMAND EXECUTED")
     return "DevSecOps STRIDE Project Running"
 if __name__ == '__main__':
     app.run(host='0.0.0', port=5000)
